41d726309adab3564df0722447acc231f07e76e7
Anastasios Chatzialexiou <130715221+bastardtoadflax@users.noreply.github.com>
2024-09-12 19:54:49 +0300

    initial clojure commit

--- roman_numerals_test.clj
+++ roman_numerals_test.clj
@@ -23,9 +23,6 @@
 (deftest nine
   (is (= "IX" (roman-numerals/numerals 9))))
 
-(deftest sixteen
-  (is (= "XVI" (roman-numerals/numerals 16))))
-
 (deftest twenty-seven
   (is (= "XXVII" (roman-numerals/numerals 27))))
 
@@ -34,9 +31,6 @@
 
 (deftest fifty-nine
   (is (= "LIX" (roman-numerals/numerals 59))))
-
-(deftest sixty-six
-  (is (="LXVI" (roman-numerals/numerals 66))))
 
 (deftest ninety-three
   (is (= "XCIII" (roman-numerals/numerals 93))))
@@ -47,17 +41,11 @@
 (deftest one-hundred-sixty-three
   (is (= "CLXIII" (roman-numerals/numerals 163))))
 
-(deftest one-hundred-sixty-six
-  (is (= "CLXVI" (roman-numerals/numerals 166))))
-
 (deftest four-hundred-two
   (is (= "CDII" (roman-numerals/numerals 402))))
 
 (deftest five-hundred-seventy-five
   (is (= "DLXXV" (roman-numerals/numerals 575))))
-
-(deftest six-hundred-sixty-six
-  (is (= "DCLXVI" (roman-numerals/numerals 666))))
 
 (deftest nine-hundred-eleven
   (is (= "CMXI" (roman-numerals/numerals 911))))
@@ -65,17 +53,5 @@
 (deftest one-thousand-twenty-four
   (is (= "MXXIV" (roman-numerals/numerals 1024))))
 
-(deftest one-thousand-six-hundred-sixty-six
-  (is (= "MDCLXVI" (roman-numerals/numerals 1666))))
-
 (deftest three-thousand
   (is (= "MMM" (roman-numerals/numerals 3000))))
-
-(deftest three-thousand-one
-  (is (= "MMMI" (roman-numerals/numerals 3001))))
-
-(deftest three-thousand-eight-hundred-eighty-eight
-  (is (= "MMMDCCCLXXXVIII" (roman-numerals/numerals 3888))))
-
-(deftest three-thousand-nine-hundred-ninety-nine
-  (is (= "MMMCMXCIX" (roman-numerals/numerals 3999))))
