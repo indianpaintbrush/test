f0be77f4e1ab53a6be279f4365bb4d4e5d87d6d8
indianpaintbrush <130715125+indianpaintbrush@users.noreply.github.com>
2024-09-13 02:40:32 +0300

    Update roman_numerals_test.clj

--- roman_numerals_test.clj
+++ roman_numerals_test.clj
@@ -23,9 +23,6 @@
 (deftest nine
   (is (= "IX" (roman-numerals/numerals 9))))
 
-(deftest sixteen
-  (is (= "XVI" (roman-numerals/numerals 16))))
-
 (deftest twenty-seven
   (is (= "XXVII" (roman-numerals/numerals 27))))
 
@@ -35,9 +32,6 @@
 (deftest fifty-nine
   (is (= "LIX" (roman-numerals/numerals 59))))
 
-(deftest sixty-six
-  (is (="LXVI" (roman-numerals/numerals 66))))
-
 (deftest ninety-three
   (is (= "XCIII" (roman-numerals/numerals 93))))
 
@@ -46,9 +40,6 @@
 
 (deftest one-hundred-sixty-three
   (is (= "CLXIII" (roman-numerals/numerals 163))))
-
-(deftest one-hundred-sixty-six
-  (is (= "CLXVI" (roman-numerals/numerals 166))))
 
 (deftest four-hundred-two
   (is (= "CDII" (roman-numerals/numerals 402))))
@@ -70,12 +61,3 @@
 
 (deftest three-thousand
   (is (= "MMM" (roman-numerals/numerals 3000))))
-
-(deftest three-thousand-one
-  (is (= "MMMI" (roman-numerals/numerals 3001))))
-
-(deftest three-thousand-eight-hundred-eighty-eight
-  (is (= "MMMDCCCLXXXVIII" (roman-numerals/numerals 3888))))
-
-(deftest three-thousand-nine-hundred-ninety-nine
-  (is (= "MMMCMXCIX" (roman-numerals/numerals 3999))))
